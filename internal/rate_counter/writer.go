ec4308fc2f20e1b83f1a96902bf84738138fb537
Malinovskii Vladimir <galqiwi@gmail.com>
2024-08-12 05:20:46 +0300

    rate_counter

---
+++ internal/rate_counter/writer.go
@@ -0,0 +1,69 @@
+package rate_counter
+
+import (
+	"io"
+	"sync"
+	"time"
+)
+
+type Rate float64
+
+// RateCountingWriter is an io.Writer that counts the number of bits written over defined intervals.
+// This allows for tracking the writing rate (bits per second) over time.
+type RateCountingWriter struct {
+	mu sync.RWMutex
+
+	intervalDuration time.Duration
+
+	lastIntervalFinish time.Time
+	lastIntervalBits   int64
+
+	thisIntervalBits int64
+}
+
+var _ io.Writer = (*RateCountingWriter)(nil)
+
+func NewRateCountingWriter(intervalDuration time.Duration) *RateCountingWriter {
+	return &RateCountingWriter{
+		intervalDuration: intervalDuration,
+
+		lastIntervalFinish: time.Now(),
+		lastIntervalBits:   0,
+
+		thisIntervalBits: 0,
+	}
+}
+
+func (r *RateCountingWriter) Write(p []byte) (n int, err error) {
+	now := time.Now()
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if now.After(r.lastIntervalFinish.Add(r.intervalDuration)) {
+		r.lastIntervalFinish = now
+		r.lastIntervalBits = r.thisIntervalBits
+		r.thisIntervalBits = 0
+	}
+
+	r.thisIntervalBits += int64(len(p))
+
+	return len(p), nil
+}
+
+func (r *RateCountingWriter) GetRate() Rate {
+	now := time.Now()
+
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	for now.After(r.lastIntervalFinish.Add(r.intervalDuration)) {
+		r.mu.RUnlock()
+		// updates interval statistics
+		_, _ = r.Write(nil)
+		now = time.Now()
+		r.mu.RLock()
+	}
+
+	return Rate(float64(r.lastIntervalBits) / r.intervalDuration.Seconds())
+}
---
+++ internal/rate_counter/writer_test.go
@@ -0,0 +1,87 @@
+package rate_counter
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
+
+func TestRateCountingWriter_Write(t *testing.T) {
+	interval := 1 * time.Second
+	rateCounter := NewRateCountingWriter(interval)
+
+	data := []byte("test data")
+	n, err := rateCounter.Write(data)
+
+	require.NoError(t, err)
+	assert.Equal(t, len(data), n)
+}
+
+func TestRateCountingWriter_Get(t *testing.T) {
+	tick := 50 * time.Millisecond
+	rateCounter := NewRateCountingWriter(2 * tick)
+
+	data := []byte("test data")
+	n, err := rateCounter.Write(data)
+	require.NoError(t, err)
+	assert.Equal(t, len(data), n)
+
+	assert.Equal(t, Rate(0), rateCounter.GetRate())
+
+	time.Sleep(3 * tick)
+
+	assert.Equal(t, Rate(float64(len(data))/(2*tick).Seconds()), rateCounter.GetRate())
+
+	time.Sleep(3 * tick)
+
+	assert.Equal(t, Rate(0), rateCounter.GetRate())
+}
+
+func TestRateCountingWriter_Concurrent(t *testing.T) {
+	tick := 50 * time.Millisecond
+	rateCounter := NewRateCountingWriter(2 * tick)
+
+	data := []byte("test data")
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			n, err := rateCounter.Write(data)
+			assert.NoError(t, err)
+			assert.Equal(t, len(data), n)
+		}()
+	}
+	wg.Wait()
+
+	time.Sleep(3 * tick)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			assert.Equal(t, Rate(float64(len(data))/(2*tick).Seconds())*10, rateCounter.GetRate())
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			n, err := rateCounter.Write(data)
+			assert.NoError(t, err)
+			assert.Equal(t, len(data), n)
+		}()
+	}
+	wg.Wait()
+
+	time.Sleep(2 * tick)
+	assert.Equal(t, Rate(float64(len(data))/(2*tick).Seconds())*10, rateCounter.GetRate())
+}

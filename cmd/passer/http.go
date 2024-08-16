0d3830310e6cf943c5d0d6b159ff25767627e94b
Malinovskii Vladimir <galqiwi@gmail.com>
2024-08-16 03:09:14 +0300

    added http limiters

--- cmd/passer/http.go
+++ cmd/passer/http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/galqiwi/fair-p/internal/ratelimit"
 	"github.com/galqiwi/fair-p/internal/utils"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
@@ -8,14 +9,26 @@ import (
 	"net/http"
 )
 
-func (run *Runner) handleHTTP(w http.ResponseWriter, req *http.Request, traceId uuid.UUID) {
+func (run *Runner) handleHTTP(w http.ResponseWriter, r *http.Request, traceId uuid.UUID) {
 	run.concurrentRequests.Add(1)
 	defer run.concurrentRequests.Sub(1)
 
-	resp, err := http.DefaultTransport.RoundTrip(req)
+	remoteHost := utils.TryGettingHostFromRemoteAddr(r.RemoteAddr)
+
+	run.logger.Info("Handling HTTP request",
+		zap.String("url", r.URL.String()),
+		zap.String("remote_host", remoteHost),
+		zap.String("trace_id", traceId.String()),
+	)
+
+	// TODO: upload limiter?
+	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
-		run.logger.Info("RoundTrip error", zap.String("url", req.URL.String()), zap.String("err", err.Error()),
-			zap.String("trace_id", traceId.String()))
+		run.logger.Info("RoundTrip error",
+			zap.String("url", r.URL.String()),
+			zap.String("err", err.Error()),
+			zap.String("trace_id", traceId.String()),
+		)
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 		return
 	}
@@ -23,14 +36,29 @@ func (run *Runner) handleHTTP(w http.ResponseWriter, req *http.Request, traceId 
 	utils.CopyHeader(w.Header(), resp.Header)
 	w.WriteHeader(resp.StatusCode)
 
-	// no limiter because no one is using http without TLS nowadays
-	bytesCopied, err := io.Copy(w, resp.Body)
+	hostLimiter := run.hostRecvLimiterStorage.GetLimiterHandle(remoteHost)
+	defer hostLimiter.CloseHandle()
+	recv, err := ratelimit.Copy(
+		io.MultiWriter(w, run.mainRecvRateCounter, run.mainRecvBitsCounter.GetCountingWriter()),
+		resp.Body,
+		[]ratelimit.Limiter{
+			ratelimit.NewCombinedLimiter(hostLimiter.Limiter, run.sharedRecvLimiter),
+			run.mainRecvLimiter,
+		},
+	)
 
 	if err != nil {
-		run.logger.Info("Error copying response body", zap.String("url", req.URL.String()), zap.String("err", err.Error()),
-			zap.String("trace_id", traceId.String()))
+		run.logger.Info("Error copying response body",
+			zap.String("url", r.URL.String()),
+			zap.String("err", err.Error()),
+			zap.String("trace_id", traceId.String()),
+		)
 		return
 	}
-	run.logger.Info("HTTP response forwarded", zap.String("url", req.URL.String()), zap.Int64("bytes_copied", bytesCopied),
-		zap.String("trace_id", traceId.String()))
+	run.logger.Info("HTTP response forwarded",
+		zap.String("remote_host", remoteHost),
+		zap.String("url", r.URL.String()),
+		zap.Int64("bits_received", recv),
+		zap.String("trace_id", traceId.String()),
+	)
 }
